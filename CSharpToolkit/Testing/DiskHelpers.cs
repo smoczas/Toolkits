729f11fca6a408d329daafcdecf3b6e39e403fd6
smoczas <52235174+smoczas@users.noreply.github.com>
2019-08-02 10:27:40 +0200

    Add DiskHelpers

---
+++ CSharpToolkit/Testing/DiskHelpers.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpToolkit.Testing
+{
+    public static class DiskHelpers
+    {
+        public static void Attach(this SeamsExt seams, Disk disk)
+        {
+            seams.DirectoryGetter = disk;
+            seams.FileGetter = disk;
+        }
+    }
+}
